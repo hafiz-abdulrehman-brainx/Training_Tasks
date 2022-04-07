91ecd7eb64515540e0f37c7c223c90738b0ed3d7
hafiz abdulrehman <hafiz.abdulrehman@brainxtech.com>
2022-04-07 17:00:14 +0500

    task 2 completed

---
+++ practice.kt
@@ -0,0 +1,25 @@
+fun main() {
+    ////////task 2 30-40 mint
+fun main() {
+
+    var list = listOf<String>("brainXTechnologies","Hello", "World", "in", "a", "frame" )
+    printList(list)
+ }
+ fun printList(list:List<String>){
+     var longest:Int = 0
+     for (element in list){
+          if(longest<element.length)
+         longest = element.length
+     }
+     var formatTemplate = "%${longest}s"
+     for(i in 1..longest+2)
+     print("*")
+     for (element in list){
+      print( "\n*"+formatTemplate.format(element)+"*") 
+     }
+     print("\n")
+     for(i in 1..longest+2)
+     print("*")
+ }
+     
+}
