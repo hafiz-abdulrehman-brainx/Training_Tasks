4f58a5cf7e96744f66498be1b869a09c4a078049
hafiz abdulrehman <hafiz.abdulrehman@brainxtech.com>
2022-04-07 17:52:38 +0500

    task 3 first attempt with 2 loops

---
+++ practice.kt
@@ -0,0 +1,26 @@
+
+////////task 3 
+fun main() {
+
+    var list = listOf<Int>(1,2,3,4,5,6,7,8 )
+    val indices=IndicesSum(list, 7)
+    println(indices.first)
+    println(indices.second)
+    
+ }
+ fun IndicesSum(list:List<Int>, x:Int):Pair<Int,Int> {
+     var indice:Int = 0
+     var indice1:Int= 0
+     for(i in 0..list.size-1){
+         for(j in 1..list.size-1){
+                if(list[i]+list[j]==x)
+                 {
+                  indice = i
+                  indice1 = j
+                  break
+                 }
+         }
+     }
+     return Pair(indice,indice1)
+ }
+ 
