923d7a06d77d6fda35e3d6cb2437b24aa1147a83
hafiz abdulrehman <hafiz.abdulrehman@brainxtech.com>
2022-04-08 16:39:17 +0500

    code updated and indented

--- practice.kt
+++ practice.kt
@@ -3,25 +3,25 @@
 fun main() {
     ////////task 2 30-40 mint
     printRectangularFrameList(listOf<String>("brainXTechnologies-brainXTechnologies","brainXTechnologies","Hello", "World", "in", "a", "frame" ))
- }
- fun printRectangularFrameList(list:List<String>){
-     var longestElement:Int = 0
+}
+fun printRectangularFrameList(list:List<String>){
+  var longestElement:Int = 0
 
-    for (element in list){
-          if(longestElement<element.length)
-          longestElement = element.length
-     }
-     var formatTemplate = "%${-longestElement}s"
+  for (element in list){
+    if(longestElement<element.length)
+      longestElement = element.length
+  }
+  var formatTemplate = "%${-longestElement}s"
 
-    for(i in 1..longestElement+2)
-        print("*")
-     
-    for (element in list){
-        print( "\n*"+formatTemplate.format(element)+"*") 
-     }
-     print("\n")
+  for(i in 1..longestElement+2)
+    print("*")
     
-    for(i in 1..longestElement+2)
-        print("*")
- }
+  for (element in list){
+    print( "\n*"+formatTemplate.format(element)+"*") 
+  }
+  print("\n")
+  
+  for(i in 1..longestElement+2)
+    print("*")
+}
      
