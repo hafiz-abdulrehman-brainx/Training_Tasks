c7bce2f5a473f7347d2673927032fd441e4d523f
hafiz abdulrehman <hafiz.abdulrehman@brainxtech.com>
2022-04-08 11:39:33 +0500

    task 3 working fine

--- practice.kt
+++ practice.kt
@@ -2,8 +2,8 @@
 ////////task 3 40, 50 min
 fun main() {
 
-    var list = listOf<Int>(1,2,3,4,5,6,7,8 )
-    val indices=IndicesSum(list, 7)
+    var list = listOf<Int>(3,2,1,4)
+    val indices=IndicesSum(list, 5)
     println(indices.first)
     println(indices.second)
     
@@ -11,16 +11,19 @@ fun main() {
  fun IndicesSum(list:List<Int>, x:Int):Pair<Int,Int> {
      var indice:Int = 0
      var indice1:Int= 0
-     for(i in 0..list.size-1){
-         for(j in 1..list.size-1){
-                if(list[i]+list[j]==x)
-                 {
+outerloop@ for(i in 0..list.size-1)
+     	{
+         for(j in 1..list.size-1)
+    		{
+//              println("comparing i= $i and j= $j and sum is ${list[i]+list[j]}")
+                if((list[i]+list[j])==x)
+                 	{
                   indice = i
                   indice1 = j
-                  break
-                 }
-         }
-     }
+                  break@outerloop
+                 	}                
+         	}
+     	}
      return Pair(indice,indice1)
  }
  
