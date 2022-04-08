6c4b9bbfc0ba4e3ba2af5954906dda870b89e0cf
hafiz abdulrehman <hafiz.abdulrehman@brainxtech.com>
2022-04-08 16:43:53 +0500

    code updated for equal indices

--- practice.kt
+++ practice.kt
@@ -1,28 +1,26 @@
 ////////task 3 40, 50 min
 fun main() {
-
-    getIndicesForSumValues(listOf<Int>(3,3,6,4), 7).apply {
-    println(first)
-    println(second)
-    }
-    
+  getIndicesForSumValues(listOf<Int>(3,3,6,4), 7).apply {
+  println(first)
+  println(second)
+  } 
  }
  fun getIndicesForSumValues(list:List<Int>, x:Int):Pair<Int,Int> {
-     var FirstValue:Int = 0
-     var SecondValue:Int= 0
-     list.apply {     
-        outerloop@ for(i in indices)
-        {
+    var FirstValue:Int = 0
+    var SecondValue:Int= 0
+    list.apply {     
+      outerloop@ for(i in indices)
+      {
         for(j in 1 until size)
         {
-            if( ( get(i)+get(j) )==x)
-                    {
-                FirstValue = i
-                SecondValue = j
-                break@outerloop
-                    }                
-            }
+          if( ( get(i)+get(j) )==x && i!=j)
+            {
+            FirstValue = i
+            SecondValue = j
+            break@outerloop
+            }                
         }
-     }
-     return Pair(FirstValue,SecondValue)
+      }
+    }
+    return Pair(FirstValue,SecondValue)
  }
