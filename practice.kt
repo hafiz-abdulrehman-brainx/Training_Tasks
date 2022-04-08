6fde66947edfbd18e785ff7fce723a0af13f2d59
hafiz abdulrehman <hafiz.abdulrehman@brainxtech.com>
2022-04-08 11:02:56 +0500

    Put the list in the parameter of the function and used meaningful name

--- practice.kt
+++ practice.kt
@@ -2,23 +2,22 @@
     
 fun main() {
     ////////task 2 30-40 mint
-    var list = listOf<String>("brainXTechnologies-brainXTechnologies","brainXTechnologies","Hello", "World", "in", "a", "frame" )
-    printList(list)
+    printRectangularFrameList(listOf<String>("brainXTechnologies-brainXTechnologies","brainXTechnologies","Hello", "World", "in", "a", "frame" ))
  }
- fun printList(list:List<String>){
-     var longest:Int = 0
+ fun printRectangularFrameList(list:List<String>){
+     var longestElement:Int = 0
      for (element in list){
-          if(longest<element.length)
-         longest = element.length
+          if(longestElement<element.length)
+          longestElement = element.length
      }
-     var formatTemplate = "%${-longest}s"
-     for(i in 1..longest+2)
+     var formatTemplate = "%${-longestElement}s"
+     for(i in 1..longestElement+2)
      print("*")
      for (element in list){
       print( "\n*"+formatTemplate.format(element)+"*") 
      }
      print("\n")
-     for(i in 1..longest+2)
+     for(i in 1..longestElement+2)
      print("*")
  }
      
