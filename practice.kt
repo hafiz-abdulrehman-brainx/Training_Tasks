053d718c5e5bd18ded317c6dc3fd741850fe894b
hafiz abdulrehman <hafiz.abdulrehman@brainxtech.com>
2022-04-08 16:18:13 +0500

    code indentation

--- practice.kt
+++ practice.kt
@@ -3,14 +3,17 @@ fun main() {
     ///////////task 1 3 min
    for(number in 1..200)
     {
-        if(number%5==0 && number%7==0)
-        println("FooBar")
+        if(
+          number%5==0 &&
+          number%7==0
+          )
+          println("FooBar")
         else if (number%5==0)
-        println("Foo")
+          println("Foo")
         else if(number%7==0)
-        println("Boo")
+          println("Boo")
         else
-        println(number)
+          println(number)
         
     }
 }
